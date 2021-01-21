6a7564d8b996da4e0a3f77ed3c267563da179311
sergey2324 <sergm2324@gmail.com>
2021-01-21 20:02:54 +0300

    почистил router

--- src/router.js
+++ src/router.js
@@ -1,7 +1,4 @@
 import {createRouter, createWebHistory} from 'vue-router'
-// import HomePage from './views/HomePage'
-// import ReusablePage from './views/ReusablePage'
-// import VuexPage from './views/VuexPage'
 import NotFound from './views/NotFound'
 import Tasks from "@/views/Tasks"
 import New from "@/views/New";
@@ -12,8 +9,6 @@ export const router = createRouter({
     { path: '/', component: Tasks },
     { path: '/new', component: New },
     { path: '/:notFound(.*)', component: NotFound }
-    // { path: '/reusable', component: ReusablePage },
-    // { path: '/vuex', component: VuexPage },
   ],
   linkActiveClass: 'active',
   linkExactActiveClass: 'active'
